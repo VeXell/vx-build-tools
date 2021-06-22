1f0af6ae99c3d9eab80f73dd9209ae7c12967c99
Viacheslav Volkov <vexell@gmail.com>
2021-06-22 19:18:44 +0100

    Fix output

--- cli.js
+++ cli.js
@@ -165,5 +165,5 @@ function msToTime(duration) {
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    return minutes + ':' + seconds;
+    return `${minutes} minutes ${seconds} seconds`;
 }
