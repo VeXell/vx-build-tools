00e46362f2304a0648fd119e20091ddb125418c7
Viacheslav Volkov <vexell@gmail.com>
2021-06-22 13:50:00 +0100

    Add debug

--- config/webpack/bootstrap.js
+++ config/webpack/bootstrap.js
@@ -1,26 +1,25 @@
 const path = require('path');
-const webpackDir = path.resolve(__dirname, './');
-// Get ENV variables
-const mode = process.env.NODE_ENV || 'development';
-const configType = process.env.CONFIG_TYPE;
-
-const requireFile = `${webpackDir}/webpack.${configType}`;
-
-if (!configType) {
-    console.error('Please set webpack CONFIG_TYPE');
-    process.exit(1);
-} else {
-    try {
-        fs.existsSync(requireFile);
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(`CONFIG_TYPE "${configType}" does not exists`);
-        process.exit(1);
-    }
-}
 
 if (!process.env.CURRENT_DIR) {
     console.error('env.CURRENT_DIR is not defined in webpack config');
+    process.exit(1);
+}
+
+if (!process.env.CONFIG_TYPE) {
+    console.error('Please set webpack env.CONFIG_TYPE');
+    process.exit(1);
+}
+
+const webpackDir = path.resolve(__dirname, './');
+const mode = process.env.NODE_ENV || 'development';
+const configType = process.env.CONFIG_TYPE;
+const requireFile = `${webpackDir}/webpack.${configType}`;
+
+try {
+    fs.existsSync(requireFile);
+} catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(`CONFIG_TYPE "${configType}" does not exists. Request file ${requireFile}`);
     process.exit(1);
 }
 
